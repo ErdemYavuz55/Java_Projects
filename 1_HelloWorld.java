1b4458c0009d71a0326ea55646fe3260073adbbf
Erdem Yavuz Hacısoftaoğlu <115773260+ErdemYavuz55@users.noreply.github.com>
2024-02-02 15:40:04 +0300

    Update 1_HelloWorld.java

--- 1_HelloWorld.java
+++ 1_HelloWorld.java
@@ -1,3 +1,17 @@
+# Hello World in Java
+
+## Introduction
+This Java application is a traditional introduction to programming, simply printing "Hello World" to the console. It demonstrates the basic structure of a Java program and how to execute simple commands.
+
+## How to Compile and Run
+To compile and run this program, you need to have Java installed on your computer.
+
+1. Open your terminal or command prompt.
+2. Navigate to the directory containing `HelloWorld.java`.
+3. Compile the program:
+4. Run the compiled program:
+5. You should see `Hello World` printed on the console.
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World!");
