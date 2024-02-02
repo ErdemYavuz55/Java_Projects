cca2b859bb3a9ea7512f6dcdc6ec031e9bf7ca0a
Erdem Yavuz Hacısoftaoğlu <115773260+ErdemYavuz55@users.noreply.github.com>
2024-02-02 16:12:29 +0300

    Update 3_TemperatureConverter.java

--- 3_TemperatureConverter.java
+++ 3_TemperatureConverter.java
@@ -1,22 +1,3 @@
-# Temperature Converter in Java
-
-## Introduction
-This application converts temperatures from Fahrenheit to Celsius. It's a simple demonstration of basic arithmetic operations and user input handling in Java.
-
-## Features
-- Converts Fahrenheit to Celsius.
-- Takes user input for the temperature in Fahrenheit.
-- Displays the converted temperature in Celsius.
-
-## How to Compile and Run
-1. Compile the source file:
-2. Run the compiled class:
-3. Enter the Fahrenheit temperature when prompted, and the program will display the corresponding Celsius temperature.
-
-## Example Usage
-- Enter temperature in Fahrenheit: `68`
-- Output: `68.0 degree Fahrenheit is equal to 20.0 in Celsius.`
-
 import java.util.Scanner;
 
 public class TemperatureConverter {
