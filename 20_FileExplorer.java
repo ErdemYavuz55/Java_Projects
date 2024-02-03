7db21c49bfe3d0d8e84dddbd0683c5b41f609a07
Erdem Yavuz Hacısoftaoğlu <115773260+ErdemYavuz55@users.noreply.github.com>
2024-02-03 11:25:03 +0300

    Update 20_FileExplorer.java

--- 20_FileExplorer.java
+++ 20_FileExplorer.java
@@ -1,1 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class FileExplorer {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a directory path: ");
+        String directoryPath = scan.nextLine();
+
+        File directory = new File(directoryPath);
+        if (directory.exists() && directory.isDirectory()) {
+            File[] fileList = directory.listFiles();
+            System.out.println("Files in directory:");
+            for (File file : fileList) {
+                System.out.println(file.getName());
+            }
+        } else {
+            System.out.println("Directory does not exist.");
+        }
+    }
+}
 
